f51e7bd3f6dbd6f8dfb40453d99c525884d45cc0
Alison Bushor <AliB@Alisons-MacBook-Pro.local>
2018-01-04 11:21:45 -0500

    Homepage CSS work

--- frontend/components/main_page.jsx
+++ frontend/components/main_page.jsx
@@ -6,10 +6,17 @@ const MainPage = () => {
   return (
 
     <section className='HomePage'>
-      <div className='SectionOne'></div>
-      <div className='SectionTwo'></div>
-      <div className='SectionThree'></div>
-    <h1> HELP</h1>
+      <div className='SectionOne'>
+        <img src="https://imageproxy.viewbook.com/df5200d2a63f7a5bb315578d413e2b2c_hd.jpg"/>
+      </div>
+    <section className='HomeLeft'>
+        <div className='SectionTwo'>
+          <img src="http://s3.amazonaws.com/pxco-dev/users/images/000/000/021/original/hover.jpg?1515079082"/>
+        </div>
+        <div className='SectionThree'>
+          <img src ="https://imageproxy.viewbook.com/85180/e08b3a3ceb9356b74dc6c6d5a35359e2_hd.jpg"/>
+        </div>
+      </section>
 
 
 
