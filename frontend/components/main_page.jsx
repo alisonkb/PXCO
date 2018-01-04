a280d2238712904c742d90d67ba3a1f0b3dadd95
Alison Bushor <AliB@Alisons-MacBook-Pro.local>
2018-01-04 14:20:45 -0500

    Add all homepage hovers

--- frontend/components/main_page.jsx
+++ frontend/components/main_page.jsx
@@ -8,17 +8,18 @@ const MainPage = () => {
     <section className='HomePage'>
       <div className='SectionOne'>
         <div className='hoverSOne'></div>
-          
       </div>
-    <section className='HomeLeft'>
-        <div className='SectionTwo'>
-          <img src="https://s3.amazonaws.com/pxco-dev/users/images/000/000/021/original/plushover.png"/>
 
-        </div>
-        <div className='SectionThree'>
 
-        </div>
-      </section>
+      <section className='HomeLeft'>
+          <div className='SectionTwo'>
+            <img src="https://s3.amazonaws.com/pxco-dev/users/images/000/000/021/original/plushover2.png"/>
+
+          </div>
+          <div className='SectionThree'>
+                    <div className='hoverSThree'></div>
+          </div>
+        </section>
 
 
 
